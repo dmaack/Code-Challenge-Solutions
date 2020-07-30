4b9d26464c8a86bcca5162d093dc5250e16f3b42
dmaack <dominiquemaack@gmail.com>
2020-07-30 13:30:49 -0600

    commit unsaved files

--- UdemyMasterClass/bigO/big_o_of_1.js
+++ UdemyMasterClass/bigO/big_o_of_1.js
@@ -1,11 +1,14 @@
 // Constant Time O(1)
     // ex: grabbing the first item in the array
+    // very scalable
 
 
 const boxes = [0,1,2,3,4,5]
 
 function logFirstTwoBoxes(boxes) {
-    console.log(boxes[0]);
-    console.log(boxes[1]);
+    console.log(boxes[0]); // O(1)
+    console.log(boxes[1]); // O(1)
 };
+
+logFirstTwoBoxes(boxes) // O(2) --> O(1)
 
