567a7e95150c784f28d99b3cbdc4f605ef0dcbfb
Dominique Maack <dominiquemaack@gmail.com>
2020-08-03 23:24:42 -0600

    moved file and changed notes

---
+++ UdemyMasterClass/bigO/big_o_cheatsheet.js
@@ -0,0 +1,34 @@
+// Big O Cheat Sheet:
+
+/* 
+
+// Big Os
+
+
+O(1) Constant - no loops
+    O(log N) Logarithmic - usually searching algorithms have log(n) if they are sorted (Binary Search) (not on )
+
+O(n) Linear - for loops, while loops
+    O(n*log(n)) Log Linear - Sorting operations usually
+
+O(n^2) Quadradic - every element in a collection needs to be compared to every other element - Two nested elements
+    O(2^n) Exponential - recursive algorithms that solve a problem of size N
+
+O(n!) - you are adding a loop for every element
+
+** Iterating through half a collection is still O(n)
+** Two separate collections: O(a + b)
+
+/* ------------------------------------------------- */
+
+/*
+
+
+What can cause time in a function?
+    Operations (+, -, *, /)
+    Comparisons (<, >, ==)
+    Looping (for, while)
+    Outside Function Call (function())
+
+
+*/
